8cb70bec0852860e40fd3ccf16b6ef15f810710d
Sergio <71974638+salvar3nga@users.noreply.github.com>
2024-02-21 15:14:08 +0100

    add hangman pole

--- src/components/hangman-figure/hangman-figure.tsx
+++ src/components/hangman-figure/hangman-figure.tsx
@@ -3,8 +3,32 @@ import styles from "./hangman-figure.module.css"
 export function HangmanFigure(){
     return (
         <div className={styles.root}>
-            Hello
-            <svg>
+            <svg 
+                className={styles.figure}
+                height={250}
+                width={200}>
+
+                {/* Pole */}
+                <line x1={60} y1={20} x2={140} y2={20}/>
+                <line x1={140} y1={20} x2={140} y2={50}/>
+                <line x1={60} y1={20} x2={60} y2={230}/>
+                <line x1={20} y1={230} x2={100} y2={230}/>
+
+
+                <circle className={styles.figurePart} cx={140} cy={70} r={20}/>
+                
+                {/* Neck */}
+
+                <line className={styles.figurePart} x1={140} y1={90} x2={140} y2={150}/>
+
+                {/* Arms */}
+                <line className={styles.figurePart} x1={140} y1={120} x2={120} y2={100}/>
+                <line className={styles.figurePart} x1={140} y1={120} x2={160} y2={100}/>
+
+                {/* Legs */}
+                <line className={styles.figurePart} x1={140} y1={150} x2={120} y2={180}/>
+                <line className={styles.figurePart} x1={140} y1={150} x2={160} y2={180}/>
+
                 
             </svg>
         </div>
