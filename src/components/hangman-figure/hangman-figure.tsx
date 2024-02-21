81ef7c8822746b8ef0b43e9d6566dc263015c60a
Sergio <71974638+salvar3nga@users.noreply.github.com>
2024-02-21 14:49:23 +0100

    delete app.css styles

---
+++ src/components/hangman-figure/hangman-figure.tsx
@@ -0,0 +1,12 @@
+import styles from "./hangman-figure.module.css"
+
+export function HangmanFigure(){
+    return (
+        <div className={styles.root}>
+            Hello
+            <svg>
+                
+            </svg>
+        </div>
+    )
+}
---
+++ src/components/hangman-figure/index.ts
@@ -0,0 +1,1 @@
+export {HangmanFigure} from "./hangman-figure"
