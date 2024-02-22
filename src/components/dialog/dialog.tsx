220703fa82c837c6b80b61c7d5a9ad4d8a66e36c
Sergio <71974638+salvar3nga@users.noreply.github.com>
2024-02-22 17:25:11 +0100

    set up dialog component

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -1,12 +1,18 @@
-import styles from "./dialog.module.css"
+import styles from "./dialog.module.css";
+interface Props {
+  message: string | undefined;
+  onContinue: () => void;
+}
 
-export function Dialog(){
-    return (
-        <div className={styles.popupContainer}>
-            <div className={styles.popup}>
-                <h2>You have Won</h2>
-                <button className={styles.popupBtn}> Play again</button>
-            </div>
-        </div>
-    )
+export function Dialog({ message, onContinue }: Props) {
+  return (
+    <div className={styles.popupContainer}>
+      <div className={styles.popup}>
+        <h2>{message}</h2>
+        <button className={styles.popupBtn} onClick={onContinue}>
+          Play again
+        </button>
+      </div>
+    </div>
+  );
 }
