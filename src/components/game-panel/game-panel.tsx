e4b8c6dfe90971f80c9c77b718d6320d04f756c1
Sergio <71974638+salvar3nga@users.noreply.github.com>
2024-02-22 17:25:30 +0100

    add eventlistener on keyboard

--- src/components/game-panel/game-panel.tsx
+++ src/components/game-panel/game-panel.tsx
@@ -1,23 +1,79 @@
-import { Dialog } from '../dialog'
-import { HangmanFigure } from '../hangman-figure'
-import { Letters } from '../letters/letters'
-import { Notification } from '../notification'
-import { WordToGuess } from '../word-to-guess'
-import styles from "./game-panel.module.css"
+import React from "react";
+import { Dialog } from "../dialog";
+import { HangmanFigure } from "../hangman-figure";
+import { Letters } from "../letters/letters";
+import { Notification } from "../notification";
+import { WordToGuess } from "../word-to-guess";
+import styles from "./game-panel.module.css";
 
+import { WORDS_DB } from "../db";
 
-export function GamePanel(){
-    return (
-        <main className={styles.gameContainer}>
-            <h1>Hangman</h1>
-            <p>Find the hidden word - Enter letter</p>
-            <section className={styles.hangmanSection}>
-                <HangmanFigure/>
-                <Letters/>
-            </section>
-            <WordToGuess/>
-            {/* <Dialog/> */}
-            {/* <Notification/> */}
-        </main>
-    )
+const word = WORDS_DB[Math.floor(Math.random() * WORDS_DB.length)];
+// const word = WORDS_DB[0].toLowerCase();
+
+const correctLetters = word.split("");
+
+const wrongLetters: string[] = [];
+const guessedCorrectly: Set<string> = new Set([]);
+
+enum GameDecision {
+  WON,
+  LOST,
+}
+
+const winMessage = "You have Won, congratulations!";
+
+export function GamePanel() {
+  const [chosenLetter, setChosenLetter] = React.useState<string>("");
+  const [correctGuess, setCorrectGuess] = React.useState<string[]>([]);
+  const [gameMessage, setGameMessage] = React.useState<string | undefined>(undefined);
+  const [displayPopup, setDisplayPopup] = React.useState(false);
+
+  window.addEventListener("keydown", (e: KeyboardEvent) => {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+      setChosenLetter(e.key);
+    }
+  });
+
+  React.useEffect(() => {
+    onWordMatch(chosenLetter);
+  }, [chosenLetter]);
+
+  function onWordMatch(w: string): void {
+    if (correctLetters.includes(w)) {
+      guessedCorrectly.add(w);
+
+      setCorrectGuess(Array.from(guessedCorrectly));
+    }
+
+    if (guessedCorrectly.size === correctLetters.length) {
+      displayMessage(GameDecision.WON);
+      displayPopup;
+    }
+  }
+
+  function displayMessage(decision: GameDecision) {
+    if (decision === GameDecision.WON) {
+      setGameMessage(winMessage);
+    }
+  }
+
+  function onContinue() {
+    setGameMessage(undefined);
+    setDisplayPopup(false);
+  }
+
+  return (
+    <main className={styles.gameContainer}>
+      <h1>Hangman</h1>
+      <p>Find the hidden word - Enter letter</p>
+      <section className={styles.hangmanSection}>
+        <HangmanFigure />
+        <Letters />
+      </section>
+      <WordToGuess word={correctLetters} alreadyCorrect={correctGuess} />
+      {displayPopup && <Dialog message={gameMessage} onContinue={onContinue} />}
+      {/* <Notification/> */}
+    </main>
+  );
 }
--- src/components/letters/letters.tsx
+++ src/components/letters/letters.tsx
@@ -1,12 +1,10 @@
-import styles from "./letters.module.css"
+import styles from "./letters.module.css";
 
-export function Letters(){
-    return (
-            <div className={styles.wrongLetters}>
-                <p className={styles.uppercase}>Wrong</p>
-                <span>
-                    a,g,o,p,t
-                </span>
-            </div>
-    )
+export function Letters() {
+  return (
+    <div className={styles.wrongLetters}>
+      <p className={styles.uppercase}>Wrong</p>
+      <span>a,g,o,p,t</span>
+    </div>
+  );
 }
